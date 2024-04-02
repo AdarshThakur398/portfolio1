a80c5f2e3410903124b6d8ae90f26d4fdef51f63
AdarshThakur398 <nutius90@gmail.com>
2024-04-02 16:11:03 +0530

    ff

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base:"/portfolio1/"
+=======
+>>>>>>> parent of ef0ecb1 (workflows)
 })
