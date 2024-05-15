ef2926c4312d78143f9933c1cdc41a11d75ac581
AdarshThakur398 <nutius90@gmail.com>
2024-05-15 23:04:42 +0530

    added scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import "./app.scss"
+import "./app.scss";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/Navbar/navbar"
-import Test from "./Test"
+import Navbar from "./components/Navbar/navbar";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
+import { BrowserRouter } from "react-router-dom";
+import { useRef } from "react";
 
 const App = () => {
-  return <div>
-        <Cursor/> 
-         <section id="Homepage">
-          <Navbar/>
-         <Hero/></section> <Cursor/> 
-      <section><Parallax type="services"/></section> 
-      <Cursor/> 
-      <section id="Skills"><Services/></section>
-      <Cursor/> 
-       <section id="Projects"> <Parallax type="Projects"/> </section>
-       <Cursor/> 
-    <Portfolio/> 
-      <Cursor/>
-         <section id="Contacts"><Contact/></section>
-         <Cursor/>   
-    </div> 
+    const section1 = useRef();
+    const section2= useRef();
+    const section3=useRef();
+
+    return (
+        <BrowserRouter>
+            <div>
+                <Cursor />
+                <section id="Homepage">
+                    <Navbar section1={section1} section2={section2} section3={section3} />
+                    <Hero section1={section1} section2={section2}  section3={section3}/>
+                </section>
+                <section ref={section3}><Parallax type="services" /></section>
+                <Cursor />
+                <section id="Skills"><Services /></section>
+                <Cursor />
+                <section ref={section2} id="Projects"><Parallax   type="Projects" /></section>
+                <Cursor />
+              
+                <Portfolio/>
+                <Cursor />
+                <section ref={section1} id="Contacts"><Contact /></section>
+                <Cursor />
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,10 @@
 import "./hero.scss"
 import {motion} from "framer-motion"
+import {useNavigate} from "react-router-dom";
+
+import Contact from "../contact/Contact";
+
+
 
 const sliderVariants = {
     initial: {
@@ -40,18 +45,24 @@ const textVariants = {
    
 }
 
-const Hero = () => {
+const Hero = ({section1,section2,section3}) => {
+    const ScrollHandler = (elmref) => {
+        window.scrollTo({ top: elmref.current.offsetTop, behavior: "smooth" });
+    }
+    
+    
   return (
     <div className="hero" >
         <div className="Wrapper">
+      
         <motion.div className="textContainer" variants={ textVariants } initial="initial" animate="animate">
             <motion.h2 variants={ textVariants }>ADARSH THAKUR</motion.h2>
             <motion.h1 variants={ textVariants }>WEB DEVELOPER</motion.h1>
             <motion.div  variants={ textVariants } className="buttons">
-                <motion.button variants={ textVariants } >See the latest work!</motion.button>
-                <motion.button variants={ textVariants }>Contact me</motion.button>
+                <motion.button variants={ textVariants } onClick={() => ScrollHandler(section2)}> See the latest work!</motion.button>
+                <motion.button variants={ textVariants } onClick={() => ScrollHandler(section1)}> Contact me</motion.button>
             </motion.div>
-            <motion.img variants={ textVariants }  animate="scrollButton" src="/scroll.png"></motion.img>
+            <motion.img variants={ textVariants }  animate="scrollButton" src="/scroll.png" onClick={() => ScrollHandler(section3)}></motion.img>
         </motion.div>
         <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
             MERN STACK 
