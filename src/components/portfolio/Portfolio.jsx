e68b40e07c3b56e7e3cf9525524a7bb946364faf
AdarshThakur398 <nutius90@gmail.com>
2024-08-08 18:16:44 +0530

    link

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,7 +16,7 @@ const items = [
     title: "Doom",
     image: "Screenshot 2024-08-08 175935.png",
     desc:"Doom is a cutting-edge online meeting platform built with Next.js, Tailwind CSS, and TypeScript. Designed as an alternative to Zoom, it offers a seamless and intuitive user experience for virtual meetings. With a focus on speed, simplicity, and modern web standards, Doom ensures reliable and efficient communication for users worldwide.",
-    link:"doom-six.vercel.app"
+    link:"https://doom-six.vercel.app/"
 
     
     
