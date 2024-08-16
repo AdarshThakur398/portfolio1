672dd518f0af42f6ef0c2bfce9d51921b661544e
AdarshThakur398 <nutius90@gmail.com>
2024-08-16 17:15:15 +0530

    added github logo

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
         <div className="social">
             <a href="https://www.linkedin.com/in/adarsh-thakur-33457626a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "><img src="/linkedin.png" alt=""/></a>
             <a href="https://www.instagram.com/adarsh.99i/"><img src="/instagram.png" alt=""/></a>
+            <a href="https://github.com/AdarshThakur398/"><img src="/OIP (3).jpeg" alt=""/></a>
             
         </div>
       </div>
